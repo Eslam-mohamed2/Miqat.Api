99b9a22d7a124f6cb398803d66a811cef3936f43
Eslam-Mohamed2 <em354219@gmail.com>
2026-04-07 01:44:18 +0200

    fix dockerignore

--- Miqat.Persistence/obj/Debug/net8.0/Miqat.API.AssemblyInfo.cs
+++ Miqat.Persistence/obj/Debug/net8.0/Miqat.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Miqat.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79ed7e21d8a9ed49ad8c1654e643c3cfe3695fcb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efaf2d24708b8674c5c544e2e5cf243e5be5cad2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Miqat.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Miqat.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

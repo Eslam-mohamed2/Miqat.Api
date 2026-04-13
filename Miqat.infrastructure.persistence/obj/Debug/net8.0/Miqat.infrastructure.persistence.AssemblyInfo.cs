2ef3b5403bffc85ae787ef5b588c11bdeaba1571
Eslam-Mohamed2 <em354219@gmail.com>
2026-04-13 18:48:01 +0200

    fixing

--- Miqat.infrastructure.persistence/obj/Debug/net8.0/Miqat.infrastructure.persistence.AssemblyInfo.cs
+++ Miqat.infrastructure.persistence/obj/Debug/net8.0/Miqat.infrastructure.persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Miqat.infrastructure.persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9bd49b98172490dfc00b3fcd19530567730438f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+707b9b6e9bd6483aae5f547ab9671bd3de310ca5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Miqat.infrastructure.persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Miqat.infrastructure.persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

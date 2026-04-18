c180c5d5f61ffa5cdba9d2f39748559db5e4ff90
Eslam-Mohamed2 <em354219@gmail.com>
2026-04-19 01:30:26 +0200

    Fxing the Deployment Settings

--- Miqat.infrastructure.persistence/obj/Debug/net8.0/Miqat.infrastructure.persistence.AssemblyInfo.cs
+++ Miqat.infrastructure.persistence/obj/Debug/net8.0/Miqat.infrastructure.persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Miqat.infrastructure.persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0e9f2b47481de3d53e18af7360ef012d2dc1a2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c743ea55ea99cdea1da94bd779b2a14097d8d673")]
 [assembly: System.Reflection.AssemblyProductAttribute("Miqat.infrastructure.persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Miqat.infrastructure.persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
